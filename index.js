1052775904c035a563ddaed138d170fe3b82b981
m-waleed-saeed <weedimian@gmail.com>
2025-11-01 23:57:22 +0500

    Final Tweaks InashAllah

--- index.js
+++ index.js
@@ -25,14 +25,13 @@ const app = express()
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "60mb", extended: true }));
 
-// app.use(cors({
-//   origin: ["https://www.handscollection.com", "http://localhost:5173"],
-//   methods: ["GET", "POST", 'PUT', "PATCH", "OPTIONS", "DELETE"],
-//   allowedHeaders: ['Content-Type', 'Authorization',],
-//   credentials: true,
-// }))
+app.use(cors({
+  origin: ["https://handscollection.com", "https://www.handscollection.com", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 
-app.use(cors({ credentials: true }))
 app.use(morgan("dev"))
 app.use(express.json());
 app.use(bodyParser.json())
